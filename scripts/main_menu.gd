dd99d2f3a1ae6e828a456793329c2249d25546de
AxelArredondo <axel.arredondotapia@edu.sait.ca>
2026-04-17 03:24:20 -0600

    menu update

---
+++ scripts/main_menu.gd
@@ -0,0 +1,34 @@
+extends Node
+
+@onready var camera: Camera2D = $Camera2D
+
+var current_screen = "main"
+var tween
+
+const SCREENS = {
+	"main": Vector2.ZERO,
+	"settings": Vector2(-400, 0),
+	"credits": Vector2(0, 240)
+}
+
+func switch_screen(screen):
+	print(camera.global_position)
+	if SCREENS.has(screen) and screen != current_screen:
+		current_screen = screen
+		if tween:
+			tween.kill()
+		tween = get_tree().create_tween()
+		tween.tween_property(camera, "global_position", SCREENS[screen], 0.4).set_trans(Tween.TRANS_QUAD)
+		
+
+func _on_settings_button_pressed() -> void:
+	switch_screen("settings")
+
+func _on_credits_button_pressed() -> void:
+	switch_screen("credits")
+
+func _on_back_button_pressed() -> void:
+	switch_screen("main")
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
